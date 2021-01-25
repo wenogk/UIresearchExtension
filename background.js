5948d5564ef6607bf4d1fcf5453ec889b5fd4a69
wenogk <drf325@nyu.edu>
2021-01-25 22:31:23 +0400

    draggable js

--- background.js
+++ background.js
@@ -4,6 +4,14 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.ib, {
+		file: 'Highlighter.js'
+	});
+	
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'dependancies.js'
+	});
+	
+	chrome.tabs.executeScript(tab.ib, {
 		file: 'inject.js'
 	});
 });
