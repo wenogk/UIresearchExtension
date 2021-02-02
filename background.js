64055d38342f50668c8be808bfdf8684a0946e51
wenogk <drf325@nyu.edu>
2021-02-03 00:03:48 +0400

    added theRoom library

--- background.js
+++ background.js
@@ -4,7 +4,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.ib, {
-		file: 'Highlighter.js'
+		file: 'theRoom.js'
 	});
 	
 	chrome.tabs.executeScript(tab.ib, {
